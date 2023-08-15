5f10b3ddfc32775293199123fa2903ca41e4aa84
Luke Stigdon <lstig@users.noreply.github.com>
2023-08-14 19:25:21 -0600

    simplify server

--- internal/server/main.go
+++ internal/server/main.go
@@ -1,15 +1,9 @@
 package server
 
 import (
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
-	"github.com/gofrs/uuid"
-	// "github.com/swaggest/openapi-go/openapi3"
-	"github.com/swaggest/rest/web"
-	"github.com/swaggest/swgui/v4emb"
-
-	v1book "github.com/lstig/liber/internal/api/v1/book"
-	"github.com/lstig/liber/internal/types"
+	// "github.com/gofrs/uuid"
+	"fmt"
+	"net/http"
 )
 
 var Version string = "not built correctly"
@@ -22,24 +16,13 @@ type Config struct {
 	ListenAddress string
 }
 
+func index(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "Index\n")
+}
+
 // Returns a configured gin router with all routes registered
-func NewServer(config Config) *web.Service {
-	s := web.DefaultService()
+func NewServer(config Config) *http.ServeMux {
+	http.HandleFunc("/", index)
 
-	s.OpenAPI.Info.Title = "Liber"
-	s.OpenAPI.Info.WithDescription("eBook server with OPDS support")
-	s.OpenAPI.Info.Version = Version
-
-	s.OpenAPICollector.Reflector().AddTypeMapping(uuid.UUID{}, types.Uuid())
-	s.OpenAPICollector.Reflector().InlineDefinition(uuid.UUID{})
-
-	s.Wrap(middleware.Logger)
-
-	s.Route("/api/v1", func(r chi.Router) {
-		v1book.RegisterRoutes(r)
-	})
-
-	s.Docs("/docs", v4emb.New)
-
-	return s
+	return http.DefaultServeMux
 }
