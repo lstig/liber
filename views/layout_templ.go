9406819cf830928fd72796355e88ccc6d8f6cb99
Luke Stigdon <lstig@users.noreply.github.com>
2023-12-28 21:27:43 -0700

    fix sidebar color

--- views/layout_templ.go
+++ views/layout_templ.go
@@ -161,7 +161,7 @@ func sidebar() templ.Component {
 			templ_7745c5c3_Var13 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<ul class=\"menu p-4 w-80 min-h-full bg-base-200 text-base-content\"><li><a>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<ul class=\"menu p-4 w-80 min-h-full bg-base-100 text-base-content\"><li><a>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
