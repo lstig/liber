c32a1f49b37dd2ba3813f17d9d9b9a14d98ecefc
Luke Stigdon <lstig@users.noreply.github.com>
2023-12-31 15:41:24 -0700

    try out breadcrumb

--- views/layout_templ.go
+++ views/layout_templ.go
@@ -92,20 +92,20 @@ func nav(page string) templ.Component {
 			templ_7745c5c3_Var7 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div class=\"w-full navbar bg-base-100\"><div class=\"flex-none lg:hidden\"><label for=\"sidenav\" aria-label=\"open sidebar\" class=\"btn btn-square btn-ghost\"><i class=\"icon-menu before:w-6 before:h-6\"></i></label></div><div class=\"flex-1 text-lg lg:text-2xl px-2 mx-2\">")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div class=\"w-full navbar bg-base-100\"><div class=\"flex-none lg:hidden\"><label for=\"sidenav\" aria-label=\"open sidebar\" class=\"btn btn-square btn-ghost\"><i class=\"icon-menu before:w-6 before:h-6\"></i></label></div><div class=\"flex-1 text-xl px-2 mx-2 breadcrumbs\"><ul><li>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
 		var templ_7745c5c3_Var8 string
 		templ_7745c5c3_Var8, templ_7745c5c3_Err = templ.JoinStringErrs(page)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `views/layout.templ`, Line: 25, Col: 58}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `views/layout.templ`, Line: 27, Col: 14}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var8))
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</div><div class=\"flex-none\"><div class=\"dropdown dropdown-end\"><button class=\"btn btn-ghost btn-circle\"><i class=\"icon-user before:w-5 before:h-5\"></i></button><ul tabindex=\"0\" class=\"mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-200 rounded-box w-52\"><li><a>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</li></ul></div><div class=\"flex-none\"><div class=\"dropdown dropdown-end\"><button class=\"btn btn-ghost btn-circle\"><i class=\"icon-user before:w-5 before:h-5\"></i></button><ul tabindex=\"0\" class=\"mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-200 rounded-box w-52\"><li><a>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
