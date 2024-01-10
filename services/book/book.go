aaf89bca20395c480b7f6ad2c4e69738a0822b3e
Luke Stigdon <lstig@users.noreply.github.com>
2024-01-10 03:17:44 -0500

    feat(books): add boilerplate

---
+++ services/book/book.go
@@ -0,0 +1,29 @@
+package book
+
+import (
+	"context"
+
+	"github.com/go-chi/httplog/v2"
+)
+
+type Book struct {
+	ID string
+	Title string
+	Author string
+}
+
+type BookService struct {
+	Log *httplog.Logger
+}
+
+func NewBookService(log *httplog.Logger) *BookService {
+	return &BookService{Log: log}
+}
+
+func (bs *BookService) Get(ctx context.Context, id string) (*Book, error) {
+	return nil, nil
+}
+
+func (bs *BookService) List(ctx context.Context) ([]Book, error) {
+	return nil, nil
+}
